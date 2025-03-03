f9f8e17b0e4afa0fbe4d52a1f3ab4ab7bbb299cc
robinkrv <163308237+robinkrv@users.noreply.github.com>
2025-03-03 16:39:39 +0100

    Sructure

---
+++ demo/src/main/java/fr/terroir_market/TerroirMarketApplication.java
@@ -0,0 +1,13 @@
+package fr.terroir_market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TerroirMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TerroirMarketApplication.class, args);
+	}
+
+}
